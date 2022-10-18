62c2c243ed046d37852c862f16cce64619369bc2
ggoomter <ggoomter@gmail.com>
2022-10-18 10:50:06 +0900

    마이바티스 설정정보 변경후 빈 등록

--- Board/src/main/java/com/study/config/DatabaseConfig.java
+++ Board/src/main/java/com/study/config/DatabaseConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.Resource;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -37,13 +38,22 @@ public class DatabaseConfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-//		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/**/*Mapper.xml"));
+		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+		factoryBean.setConfiguration(mybatisConfig());
         return factoryBean.getObject();
     }
 
-    @Bean
+
+
+	@Bean
     public SqlSessionTemplate sqlSession() throws Exception {
         return new SqlSessionTemplate(sqlSessionFactory());
     }
+	
+	@Bean
+	@ConfigurationProperties(prefix = "mybatis.configuration")
+    private org.apache.ibatis.session.Configuration mybatisConfig() {
+		return new org.apache.ibatis.session.Configuration();
+	}
 
 }
